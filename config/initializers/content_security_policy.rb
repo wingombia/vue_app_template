d4cadd62a75be3413810cd6912f8455b4ca24dc0
Minh Phan <pbminh2511@gmail.com>
2020-05-18 19:51:52 +0700

    Remove else part from content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -28,7 +28,5 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
-  else
-    policy.script_src :self, :https
   end
 end
